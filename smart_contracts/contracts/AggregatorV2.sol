0fe7a471ce663edf241d280083beec9a6f72a25e
ANAND0KUMAR <anandk6735@gmail.com>
2022-09-01 01:55:24 +0530

    update Aggregated solidity file

--- smart_contracts/contracts/AggregatorV2.sol
+++ smart_contracts/contracts/AggregatorV2.sol
@@ -132,3 +132,165 @@
 
 
 
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.4.22 <0.9.0;
+
+// pragma experimental ABIEncoderV2;
+
+contract Aggregator {
+    struct EvInfo {
+        string make;
+        string model;
+        uint8 batterySize;
+    }
+
+    struct EnergyRequirement {
+        uint16 id;
+        uint32 start;
+        uint32 end;
+        uint16 energyAmount;
+    }
+
+    struct Schedule {
+        uint16 reqId;
+        uint32 reward;
+        uint32[96] allocatedChargingSlots;
+    }
+
+    address owner;
+
+    mapping(address => EvInfo) evProfiles;
+
+    mapping(uint16 => EnergyRequirement) public energyRequirements;
+    mapping(address => uint16[]) userToEnergyRequirementIdsMap;
+    mapping(uint16 => address) energyRequirementIdToUserMap;
+    uint16[] public energyRequirementIdsForNextSchedule;
+    uint16 energyRequirementSequence;
+
+    mapping(address => Schedule) public schedules;
+    address[] schedulesIndex;
+
+    constructor() {
+        owner = msg.sender;
+        energyRequirementSequence = 0;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) return;
+        _;
+    }
+
+    // Manage Electric Vehicle Information
+    event EvProfileRegistered(address sender, EvInfo profile);
+
+    function registerProfile(
+        string memory make,
+        string memory model,
+        uint8 batterySize
+    ) internal {
+        EvInfo memory newProfile = EvInfo(make, model, batterySize);
+        evProfiles[msg.sender] = newProfile;
+        emit EvProfileRegistered(msg.sender, newProfile);
+    }
+
+    function deregisterProfile() external {
+        delete evProfiles[msg.sender];
+    }
+
+    function getProfile() external view returns (EvInfo memory profile) {
+        return evProfiles[msg.sender];
+    }
+
+    // Manage Energy Requirements
+    event NewEnergyRequirement(address sender, EnergyRequirement newReq);
+
+    function newEnergyRequirement(
+        uint32 start,
+        uint32 end,
+        uint16 energyAmount
+    ) external {
+        EnergyRequirement memory r = EnergyRequirement(
+            energyRequirementSequence,
+            start,
+            end,
+            energyAmount
+        );
+        energyRequirements[energyRequirementSequence] = r;
+        userToEnergyRequirementIdsMap[msg.sender].push(
+            energyRequirementSequence
+        );
+        energyRequirementIdToUserMap[energyRequirementSequence] = msg.sender;
+        energyRequirementIdsForNextSchedule.push(energyRequirementSequence);
+        energyRequirementSequence++;
+
+        emit NewEnergyRequirement(msg.sender, r);
+    }
+
+    function getMyEnergyRequirements()
+        external
+        view
+        returns (EnergyRequirement[] memory)
+    {
+        uint16[] storage myIds = userToEnergyRequirementIdsMap[msg.sender];
+        EnergyRequirement[] memory myReqs = new EnergyRequirement[](
+            myIds.length
+        );
+        for (uint16 i = 0; i < myIds.length; i++) {
+            myReqs[i] = energyRequirements[myIds[i]];
+        }
+        return myReqs;
+    }
+
+    // Schedule Computations
+    event NewSchedule();
+
+    function getEnergyRequirementsForNextSchedule()
+        external
+        view
+        returns (EnergyRequirement[] memory)
+    {
+        EnergyRequirement[] memory nextScheduleReqs = new EnergyRequirement[](
+            energyRequirementIdsForNextSchedule.length
+        );
+        for (
+            uint16 i = 0;
+            i < energyRequirementIdsForNextSchedule.length;
+            i++
+        ) {
+            nextScheduleReqs[i] = energyRequirements[
+                energyRequirementIdsForNextSchedule[i]
+            ];
+        }
+        return nextScheduleReqs;
+    }
+
+    function storeSchedule(Schedule[] memory newSchedules) public onlyOwner {
+        // Empty old schedules
+        for (uint256 i = 0; i < schedulesIndex.length; i++) {
+            address u = schedulesIndex[i];
+            delete schedules[u];
+        }
+        delete schedulesIndex;
+
+        // Add new schedules
+        for (uint256 i = 0; i < newSchedules.length; i++) {
+            address user = energyRequirementIdToUserMap[newSchedules[i].reqId];
+            schedules[user] = newSchedules[i];
+            schedulesIndex.push(user);
+        }
+
+        // Reset Energy Requirements for next Schedule
+        delete energyRequirementIdsForNextSchedule;
+
+        emit NewSchedule();
+    }
+
+    // Schedule Retrieval (Frontend)
+    function retrieveMyCurrentSchedule()
+        external
+        view
+        returns (Schedule memory schedule)
+    {
+        return schedules[msg.sender];
+    }
+}
